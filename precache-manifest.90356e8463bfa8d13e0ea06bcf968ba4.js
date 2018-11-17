2ac8cab2e43a7123323ecd6df638723dcb1526b2
yaser-alazem <yaser.azem@hotmail.com>
2018-11-17 02:53:57 +0200

    Updates

--- precache-manifest.90356e8463bfa8d13e0ea06bcf968ba4.js
+++ precache-manifest.90356e8463bfa8d13e0ea06bcf968ba4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-movies-series-info/static/js/runtime~main.ebccf779.js"
   },
   {
-    "revision": "15bceea25ef7f88f80d3",
-    "url": "/react-movies-series-info/static/js/main.15bceea2.chunk.js"
+    "revision": "51ea0c6c91301fc7cd1f",
+    "url": "/react-movies-series-info/static/js/main.51ea0c6c.chunk.js"
   },
   {
     "revision": "c41a9194750e4418305a",
     "url": "/react-movies-series-info/static/js/1.c41a9194.chunk.js"
   },
   {
-    "revision": "15bceea25ef7f88f80d3",
+    "revision": "51ea0c6c91301fc7cd1f",
     "url": "/react-movies-series-info/static/css/main.0418439d.chunk.css"
   },
   {
-    "revision": "c1df7df55edf18e407bda5caeebb8fbf",
+    "revision": "d95f6365ed0867bb83d1ac873e1346b5",
     "url": "/react-movies-series-info/index.html"
   }
 ];
