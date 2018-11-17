5ba26878f7c75744051325c755255e0a59b07e5a
yaser-alazem <yaser.azem@hotmail.com>
2018-11-17 03:03:15 +0200

    adding composeWithDevTools instead of hard write the redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 //Here we hold all our states
 
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -8,19 +9,10 @@ const middleware = [thunk];
 
 const initialState = {};
 
-const devTools =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null;
-
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    devTools
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
